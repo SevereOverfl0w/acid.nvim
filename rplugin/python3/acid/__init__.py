b6069c8768626f3780307e807db424156070515f
Henry Kupty <hkupty@gmail.com>
2016-10-20 09:59:52 -0200

    Let acid manage repls

--- rplugin/python3/acid/__init__.py
+++ rplugin/python3/acid/__init__.py
@@ -14,6 +14,7 @@ class Acid(object):
     def __init__(self, nvim):
         self.nvim = nvim
         self.sessions = SessionHandler()
+        self.repls = {}
         self.handlers = {}
         self._init = False
 
@@ -31,7 +32,9 @@ class Acid(object):
         [init_var(i, j)
          for i, j
          in [('acid_auto_require', 1),
-             ('acid_namespace', 'user')]]
+             ('acid_namespace', 'user'),
+             ('acid_start_repl_fn', 'jobstart'),
+             ('acid_start_repl_args', ['lein repl'])]]
 
     def init_handlers(self):
         for path in find_extensions(self.nvim, 'handlers'):
@@ -88,3 +91,16 @@ class Acid(object):
     def acid_require(self):
         data = "(require '[{} :refer :all])".format(path_to_ns(self.nvim))
         self.eval({"code": data}, "Ignore")
+
+    @neovim.command("AcidStartRepl", bang=True)
+    def acid_init_repl(self, bang):
+        nv = self.nvim
+        # explicitly requiring tab cwd
+        pwd = nv.funcs.getcwd(-1, 0)
+
+        if pwd not in self.repls or bang:
+            fn = nv.vars['acid_start_repl_fn']
+            args = nv.vars['acid_start_repl_args']
+            nv.call(fn, *args)
+        else:
+            nv.command('echom "REPL is already open"')
